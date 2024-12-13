32bfc947cbc427cdc25fdd85ab187e8fad1fe99c
ec23102@qmul.ac.uk <23102@qmul.ac.uk>
2024-12-13 15:23:55 +0000

    fixed number

--- unitTest.py
+++ unitTest.py
@@ -1,14 +1,14 @@
 import unittest
 from addNum import addNumber
-#new edit
+#updated numbers
 class testFunc(unittest.TestCase):
 
     def test_positve(self):
-       result = addNumber(3, 8)
-       self.assertEqual(result, 3)
+       result = addNumber(3, 3)
+       self.assertEqual(result, 6)
 
     def test_negative(self):
-        result = addNumber(-34, -17)
-        self.assertEqual(result, -34)
+        result = addNumber(1, 1)
+        self.assertEqual(result, 2)
 if __name__ == '__main__':
     unittest.main()

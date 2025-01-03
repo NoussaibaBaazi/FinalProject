89adb01fd857ca67df1f7ad724b48b1e4b775809
ec23102@qmul.ac.uk <23102@qmul.ac.uk>
2025-01-03 13:59:58 +0000

    new datset added, check if read correclty

--- TestReadData.py
+++ TestReadData.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 import os
-from Qualifications_graph import read_qualifications_data
+##from Qualifications_graph import read_qualifications_data
 
 class TestLibraryImports(unittest.TestCase):
 
@@ -11,13 +11,6 @@ class TestLibraryImports(unittest.TestCase):
             import pandas as pd
         except ImportError:
             self.fail("pandas is not imported correctly.")
-    
-    def test_numpy_import(self):
-        """Check if numpy is imported."""
-        try:
-            import numpy as np
-        except ImportError:
-            self.fail("numpy is not imported correctly.")
     
     def test_matplotlib_import(self):
         """Check if matplotlib is imported."""
@@ -43,14 +36,6 @@ class TestCSVReading(unittest.TestCase):
         except Exception as e:
             self.fail(f"Reading CSV failed with exception: {e}")
 
-    def test_csv_columns_present(self):
-        """Check if specific columns are present in the CSV file."""
-        file_path = 'QualificationsNVQ.csv'  
-        df = pd.read_csv(file_path)
-        required_columns = ['Code','Area','Year','% with NVQ4+ - aged 16-64: percent','% with no qualifications - aged 16-64: percent','% with NVQ2 only - aged 16-64: percent','% with NVQ3 only - aged 16-64: percent']  # Update with your column names
-        for col in required_columns:
-            self.assertIn(col, df.columns, f"Column '{col}' is missing in the dataset.")
-
 class TestCSVReadingUnemployment(unittest.TestCase):
 
     def test_csv_file_exists(self):
@@ -67,14 +52,23 @@ class TestCSVReadingUnemployment(unittest.TestCase):
         except Exception as e:
             self.fail(f"Reading CSV failed with exception: {e}")
 
-    def test_csv_columns_present(self):
-        """Check if specific columns are present in the CSV file."""
-        file_path = 'annual-unemployment-region.csv'  
-        df = pd.read_csv(file_path)
-        required_columns = ['Year','Location','numerator','denominator','percent','conf','male numerator','male denominator',
-                            'male percent','male conf','female numerator','female denominator','female percent','female conf']
-        for col in required_columns:
-            self.assertIn(col, df.columns, f"Column '{col}' is missing in the dataset.")
+
+class TestCSVReadingWorkforce(unittest.TestCase):
+
+    def test_csv_file_exists(self):
+        """Check if the CSV file exists."""
+        file_path = 'workforce-jobs-ons.csv' 
+        self.assertTrue(os.path.exists(file_path), "CSV file does not exist.")
+    
+    def test_csv_read_successfully(self):
+        """Check if the CSV file can be read without errors."""
+        file_path = 'workforce-jobs-ons.csv' 
+        try:
+            df = pd.read_csv(file_path)
+            self.assertIsInstance(df, pd.DataFrame, "Data read is not a DataFrame.")
+        except Exception as e:
+            self.fail(f"Reading CSV failed with exception: {e}")
+
 
 class TestPercentRangesQualifications(unittest.TestCase):
     def test_column_value_range(self):
